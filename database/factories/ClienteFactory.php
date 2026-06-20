cb2b015e829835989eaf4a836757d60f5e537c73
Daryl Campos <dcamposd2@miumg.edu.gt>
2026-06-20 11:44:36 -0600

    creacion de primer factory

---
+++ database/factories/ClienteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\clients;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<clients>
+ */
+class ClienteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'mail' => fake()->email(),
+            'phone'=>fake()->phoneNumber(),
+            'street' => fake()->streetAddress(),
+            'avenue' => fake()->address(),
+        ];
+    }
+}
